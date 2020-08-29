412bee9d4cb6b326037c75c3b60980187eea7821
wahyu setiawan <wahyu.creator911@gmail.com>
2020-08-29 19:33:32 +0700

    update fix

--- build.rs
+++ build.rs
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-fn main()->Result<(), Box<dyn std::error::Error>>{
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("proto/say.proto").unwrap();
     Ok(())
 }
-=======
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/helloworld.proto")?;
-    tonic_build::compile_protos("proto/user.proto")?;
-    return Ok(());
-}
->>>>>>> 7f6a2292e9bdf48fda7c93093588515a7ef25fc4
